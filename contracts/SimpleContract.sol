3b3b1da14a4b53e060a321b628d6542ad7ecbd68
YFCODER96 <yfcoder@proton.me>
2025-03-21 23:47:39 +0800

    增加数组和结构体

--- contracts/SimpleContract.sol
+++ contracts/SimpleContract.sol
@@ -15,7 +15,7 @@ contract SimpleContract {
     uint256 myFavoriteNumber = 55;
 
     // 2)函数类型
-    // 函数声明 使用function关键字 
+    // 函数声明 使用function关键字
     function store(uint256 num) public {
         // 函数名称store 可以自定义 这里的store表示仓储的意思
         // 参数名num 根据需要可以取任何变量名
@@ -27,12 +27,31 @@ contract SimpleContract {
         myFavoriteNumber = num; // 更改变变量的状态 会产生gas费用
     }
 
-    function retrieve() public view returns(uint256){
+    function retrieve() public view returns (uint256) {
         // 函数名 retrieve 指取回的意思
         // 函数的修饰符：
         // view 该函数只有读取状态时可以访问到，不会修改状态。调用view关键字的函数不消耗gas费用，但在消耗gas费用的函数中调用除外。
         // pure 该方法不改变任何状态。
         // returns(uint256) 返回值的类型
         return myFavoriteNumber;
+    }
+
+    // 3) 数组和结构体
+    struct Person {
+        string name; // 字符串
+        uint8 age; // 无符号整形
+    }
+
+    Person p1 = Person("yfcoder", 28); 
+    Person p2 = Person({name: "Duo", age: 26});
+
+    Person[] public people; //可以动态增加元素
+    Person[2] twoPerson; //不能使用push，固定了数组的大小
+
+    function addPerson(string memory _name,uint8 _age) public {
+        // string 是一个动态大小的引用类型，存储在内存中，因此需要显式地使用 memory 关键字来指明它的存储位置。
+        // uint8 是一个基本数据类型，存储在栈中，不需要指定存储位置。它的生命周期仅限于函数调用期间，因此不需要使用 memory 或 storage 关键字。
+        Person memory person = Person(_name,_age);
+        people.push(person);
     }
 }
